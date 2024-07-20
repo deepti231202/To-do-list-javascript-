9e96aa401408d48d2f73cebf1152a8e185fb9e87
DEEPTI S <142494880+deepti231202@users.noreply.github.com>
2024-07-20 16:18:24 +0530

    Add files via upload

---
+++ javascript/javascript.js
@@ -0,0 +1,16 @@
+// function add(){
+// var todoForm = document.todo.add.value;
+// var todoInput = document.todo.delete.value;
+//     var newTask = todoInput.value;
+  
+//     if (newTask === '') {
+//         alert('Please enter a task!');
+//         return;
+//     }
+//     todoInput.value = ''; 
+//   };
+
+// function delete (){
+
+// }
+
